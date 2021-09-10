1f7d6a50990d2fe3642584680282ee6b66169093
sergkhr <serg.khrechko@gmail.com>
2021-09-10 11:17:52 +0300

    made first practice folder

--- First Practice/Arifm.cpp
+++ First Practice/Arifm.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <windows.h>
+#include <locale.h>
 
 using namespace std;
 
 int main() {
     //окей, мне нужна помощь, как сделать вывод кириллицы, советы от гугла не помогают
+    setlocale(LC_ALL, "rus");
 	cout << "введите два числа" << endl;
     double a, b;
     cin >> a >> b;
-    
+
 
     cout << a+b << endl;
     cout << a-b << endl;
